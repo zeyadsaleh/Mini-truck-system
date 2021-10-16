86c81e17b0edcaf500ca89a68b1594e239789531
Zeyad Saleh <zeyadmostafasaleh@gmail.com>
2021-10-16 17:54:49 +0200

    perform sidekiq worker every 1 day

--- app/workers/truck_report_worker.rb
+++ app/workers/truck_report_worker.rb
@@ -1,9 +1,11 @@
 require 'sidekiq-scheduler'
-class TruckReportWorker
+class TruckReportWorker 
   include Sidekiq::Worker
 
   def perform(*args)
-    distance_sql = "SELECT truck_id, ST_Length(ST_MakeLine(geom::geometry)::geography) as distance, (COUNT(*) * 0.5) as active_time_in_minutes, FROM truck_logs WHERE created_at >'" + 1.days.ago.to_s + "' GROUP by truck_id"
-    trucks_daily_report = ActiveRecord::Base.connection.execute(distance_sql)
+    distance_sql = "SELECT truck_id, ST_Length(ST_MakeLine(geom::geometry)::geography) as distance, (COUNT(*) * 0.5) as active_time_in_minutes, ((COUNT(*) * 0.5 / 60)) as active_time_in_hours FROM truck_logs WHERE created_at >(SELECT NOW() - INTERVAL '1 DAY') GROUP by truck_id"   #since the truck logs every 30 seconds so the active time in minutes of the truck is truck log records count * 0.5 :)
+    trucks_daily_report = ActiveRecord::Base.connection.execute(distance_sql).to_a #use this to build the report 
+    puts trucks_daily_report
+    return trucks_daily_report
   end
 end
