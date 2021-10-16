10ac5c84c3e79033a51922df63ddfebcc756359d
Zeyad Saleh <zeyadmostafasaleh@gmail.com>
2021-10-16 16:40:16 +0200

    daily report

--- app/workers/truck_report_worker.rb
+++ app/workers/truck_report_worker.rb
@@ -3,7 +3,7 @@ class TruckReportWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    distance_sql = "SELECT truck_id, ST_Length(ST_MakeLine(geom::geometry)::geography) as distance FROM truck_logs WHERE created_at >'" + 1.days.ago.to_s + "' GROUP by truck_id"
-    total_distance_travelled = ActiveRecord::Base.connection.execute(distance_sql)
+    distance_sql = "SELECT truck_id, ST_Length(ST_MakeLine(geom::geometry)::geography) as distance, (COUNT(*) * 0.5) as active_time_in_minutes, FROM truck_logs WHERE created_at >'" + 1.days.ago.to_s + "' GROUP by truck_id"
+    trucks_daily_report = ActiveRecord::Base.connection.execute(distance_sql)
   end
 end
